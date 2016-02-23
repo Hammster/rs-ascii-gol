8a1ca56cce37214382ca332e55b1bd38f8fb0c17
Hammster <mail@hans-koch.me>
2016-02-23 01:31:41 +0100

    Add args, Refactor loop

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate rand;
 use std::env;
 use std::sync::{Arc, Mutex};
 use std::io;
+use std::str::FromStr;
 
 use grid::Grid;
 
@@ -12,41 +13,27 @@ mod seeds;
 
 fn main() {
 
-    let mut input = String::new();
-
     let seed = env::args().nth(1).map(|s|
         seeds::named(&s).expect("Invalid seed name! Valid seeds are random or gosper_glider")
-    ).unwrap_or(seeds::random);
+    ).unwrap_or(seeds::gosper_glider);
 
+    let max_gen = env::args().nth(2).map(|s|
+        u32::from_str(&s).expect("Only numbers!")
+    ).unwrap_or(1);
 
-    let grid = Arc::new(Mutex::new(Grid::new(seed, 128, 50)));
+    let grid = Arc::new(Mutex::new(Grid::new(seed, 30, 15)));
     let mut gen = 1;
+
+    println!("{}",max_gen);
 
     grid.lock().unwrap().draw();
 
-    loop{
-        println!("How many generations ?");
-
-        io::stdin()
-        .read_line(&mut input)
-        .expect("failed to read from stdin");
-
-        let trimmed = input.trim();
-
-        match trimmed.parse::<u32>() {
-            Ok(i) => {
-                for _ in 0..i {
-                    gen += 1;
-
-                    grid.lock().unwrap().update();
-                    grid.lock().unwrap().draw();
-                }
-                println!("({}th gen)",gen);
-                //grid.lock().unwrap().draw();
-            },
-            Err(..) => return
-        };
-
+    for _ in 0..max_gen {
+        if(gen == max_gen){
+            return;
+        }
+        gen += 1;
+        grid.lock().unwrap().update();
+        grid.lock().unwrap().draw();
     }
-
 }
